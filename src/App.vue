27c2efdc71b03bde4bba7edd4b699a3d1c5c28d4
Victor Kane <victorkane@gmail.com>
2017-08-15 21:38:13 -0300

    Initial desktop menu as per lesson 4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon>
+          View Meetup
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+    </main>
   </v-app>
 </template>
 
